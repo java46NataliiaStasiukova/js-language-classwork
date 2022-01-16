eecd936bffd374866af30045e3db70e8b9215b2c
Nataliia <nstasiukova@gmail.com>
2022-01-16 23:20:02 +0200

    classwork-11

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
-console.log("Hello World");
-//for (var i = 0; i < 3; i++ ){
- //   setTimeout(function(){
-  //      console.log(i);
-   // })
-//}
-//for(let i = 0; i < 3; i ++){
-   // setTimeout(function(){
-    //    console.log(i);
-   // })
-//}
-//for (var i = 0; i < 3; i++ ){
- //   console.log(i);
-//}
-//console.log(i);
-const fun = function(a, b){
-    return a + b;
+// ********************Strings
+const str1 = "abc";
+const str2 = "abc";
+console.log(`str1 == str2 is ${str1 == str2}`);
+console.log(`str1 < "s" is ${str1 < "s"}`);
+console.log(`"abc" < "ab" is ${str1 < "ab"}`); //false
+console.log(`"123" > 23 is ${"123" > 23}`);
+console.log(`"123" > "23" is ${"123" > "23"}`);
+// ********************
+//if logical expression containes - both string and number, JS will convert string to number;
+//if string doesn't contain a result of convertion will be NaN;
+//In any logical expression if there is NaN, then result will be false;
+console.log(`"abc" > 23 is ${"abc" > 23}`);
+console.log(`"abc" < 23 is ${"abc" < 23}`);
+console.log(`"abc" != 23 is ${"abc" != 23}`);
+console.log(`"abc" != 23 is ${!("abc" == 23)}`);
+//**********************
+function stringProcessing(str){
+   const strP = "" + str;
+   console.log(`"${str}"[4] is ${str[4]}`);
+   console.log(`length of "${str}" is ${str.length}`);
+   console.log(`"${str}" includes "abc" is ${strP.includes("abc")}`);
+   console.log(`substring of "${str}" beginning from index 2 to index 5 is ${strP.substring(2, 5)}`);
+   console.log(`index of substring "ll" in the "${str}" is ${strP.indexOf("ll")};
+    index of substring ll in the "${str}" is ${strP.indexOf("ll")};
+    last index of substring ll in the "${str}" is ${strP.lastIndexOf("ll")}`);
 }
-let a = 5;
-//console.log(a ** 2);
-//console.log(fun(10, 20));
+//stringProcessing("abcdt lmn ll!@ oo ttll");
+function encode(num, base){
+   //base from 2 to 10;
+   let res = " ";
+   do{
+         const digit = Math.trunc(num % base);
+         const symb = getSymbol(digit);
+         res = symb + res;
+         num = Math.trunc(num / base);
+   }while(num >= 1);
+   return res;
+}
+function getSymbol(digit){
+   //base from 2 to 10
+   return "" + digit; //it will work only for base <- 10
+}
+console.log(encode(10, 2));
 
-//console.log(a(10,20));//error
-//console.log(fun ** 2);//NaN
-console.log("12" + 12, '"12" + 12');//console.log("12" + 12); // 1212 - JS defines that operator + exists 
-//for string, that's why number 12 will be converted to string
-console.log("12" - 12);//JS defines that operator - doesn't exist for string, 
-//that's why string "12" will be converted to NaN
-console.log("ab" - 12); //.....JS will try conver "ab" 
-//a result of this there will ve value Nan.
-
-//Sometimes there is a need to explicit convertion from string to a number
-//"+" unary is simplest way for the explicit conversion of a string to a number
-console.log(`+"12" + 12 = ${+"12" + 12}`);
