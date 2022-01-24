d188af400457e3001a41f78fb8eabda3bd997437
Nataliia <nstasiukova@gmail.com>
2022-01-24 19:42:58 +0200

    Classwork-14.1

--- app.js
+++ app.js
@@ -1,51 +1,25 @@
-function mapper(n, i, a){
-   return `<li>${i + 1} of ${a.length} - ${n}</li>`;
-}
-function someFunction(){
-   const ar = [-10, 50, -12, 80, 40];
-   const ar3 = ar.map(mapper);
-   console.log(ar3);
-}
+// Classwork part-1
+// Filter
+const ar20 = [13, 17, 20, 23, 2, 40];
+const arEven = ar20.filter((n, i, a) => n % 2 == 0);
+const arEvenOdd = ar20.filter((n, i, a) => ar20.length % 2 == 0 ? 
+n % 2 == 0 : n % 2 == 1);
+console.log(arEvenOdd);
 
-const ar = [-10, 50, -12, 80, 40];
-ar.push(70);
-//1 ****** str - "-10#50#-12#80#40#70"
-//**********solution based on substring  */
-//let str = '';
-//ar.forEach(function(n){
-//   str += n + '#';
-//})
-// str - "-10#50#-12#80#40#70"
-//str = str.substring(0, str.length - 1);
+// Reduce
+let res = ar20.reduce((res, cur) => res + cur, 0);
+console.log(res);
+const max = ar20.reduce((max, cur) => cur > max ? cur : max, ar20[0]);
+console.log(max);
+//reduce with no second element
+res = ar20.reduce((res, cur) => res + cur);
+console.log(res);
+//if the user call does not contain a second argument, 
+//then the first element of the array will be considered as 
+//an initial result ( in the case iterating begins from the 
+//second element of the array)
 
-// str - "-10#50#-12#80#4070"
-//const lastSharpIndex = str.lastIndexOf('#')
-//const str1 = str.substring(0, lastSharpIndex);
-//const str2 = str.substring(lastSharpIndex +1);
-//str = str1 + str2;
 
-//**********solution based on forEach from second number */
-// str - "-10#50#-12#80#40#70"
-//const ar1 = ar.slice(1);
-//let str =  '' +ar[0];
-//ar1.forEach(n => str += '#' + n);
-//console.log(str);
-
-//***********printing out sequense number of element, element, length of array */
-// 1 of 5 => -10; .... 2 of 5 => 50 ....
-//ar.forEach((n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
-
-//********************************************************** */
-//method "map"
-//use case of applying method map: you want to create new array with elements that are recived as result of some converton
-//example you want to create new array with elements that are multiplication on 2 of each source element
-const ar2 = ar.map(n => n * 2);
-console.log(ar2);
-someFunction();
-//const ar3 = ar.map(n => `<li>${n}</li>`);
-
-//const ar3 = ar.map()
-//console.log(ar3);
 
 
 
