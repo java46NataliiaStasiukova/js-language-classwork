8496c177d72fc57a5600375c3a0660f2da1f3de2
Nataliia <nstasiukova@gmail.com>
2022-01-25 01:17:39 +0200

    Classwork-14.2

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-// Classwork part-1
-// Filter
-const ar20 = [13, 17, 20, 23, 2, 40];
-const arEven = ar20.filter((n, i, a) => n % 2 == 0);
-const arEvenOdd = ar20.filter((n, i, a) => ar20.length % 2 == 0 ? 
-n % 2 == 0 : n % 2 == 1);
-console.log(arEvenOdd);
 
-// Reduce
-let res = ar20.reduce((res, cur) => res + cur, 0);
-console.log(res);
-const max = ar20.reduce((max, cur) => cur > max ? cur : max, ar20[0]);
-console.log(max);
-//reduce with no second element
-res = ar20.reduce((res, cur) => res + cur);
-console.log(res);
-//if the user call does not contain a second argument, 
-//then the first element of the array will be considered as 
-//an initial result ( in the case iterating begins from the 
-//second element of the array)
+const person1 ={id: 123, name: 'Moshe', address: {city: 'Lod', street: 'Sokolov'}};
+const person2 ={id: 123, name: 'Moshe', address: {city: 'Lod', street: 'Sokolov'}};
+const person3 = person1;
+console.log(`person1 = person2 is ${person1 == person2}`);
+console.log(`"123" == 123 is ${"123" == 123}`);
+console.log(`"123" === 123 is ${"123" === 123}`);
+console.log(`person1 = person3 is ${person1 == person3}`);
+console.log(`JSON.stringify(person1) === JSON.stringify(person2) is ${
+   JSON.stringify(person1) === JSON.stringify(person2) }`);
+console.log(JSON.stringify(person1));
+console.log(person1.toString());
+console.log(`name of person1 is ${person1.name}`);
+console.log(`person1 lives in city ${person1.address.city}`);
+Object.keys(person1).forEach(k => console.log(k));//array of the object keys
+Object.values(person1).forEach(v => console.log(v));//array of the object values
+Object.entries(person1).forEach(e => console.log(e));//array of arrays - [key, value];
+console.log(Object.entries(person1));
 
-
+function createAddress(city, street){
+   //{city: city, street: street}  <=>  {city, street}
+   return {city: city, street: street};
+}
+function createPerson(id, name, address){
+   return {id, name, address};
+}
+const persons = [
+   createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
+   createPerson(124, "Olya", createAddress("Rehovot", "Pr. Plaut")),
+   createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
+]
 
 
 
