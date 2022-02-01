95dfc5af95dc60b8358d32141cadaac56f4ffa81
Nataliia <nstasiukova@gmail.com>
2022-02-01 02:47:22 +0200

    Classwork-16-OOP

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-// input: ["lmn", "d", "d", "lmn", "a", "lmn", "a", "bc"]
-// output: lmn -> 3
-//           a -> 2
-//           d -> 2
-//          bc -> 1
-function displayOccurrences(array){
-    //creating object with key - unique element of array (string as an element of array)
-    //                     value - occurrences count 
-    // difference between obj = {a: 123, d: "abc"}; const a = "d"; obj.a == 123
-    // and obj[a] === "abc";
-    //obj.c = 10 -> {a: 123, d: "abc", c: 10}
-    const res = {};
-    for(let i = 0; i < array.length; i++){
-        if(res[array[i]] === undefined){
-            //string as content ao array[i] occures first tim
-            res[array[i]] = 1;
-        }
-        else{
-            res[array[i]] = res[array[i]] +1;
-        }
-    }
-   // console.log(res);// -> intermediate debug log
-   Object.entries(res).sort((e1, e2) => {
-    const res = e2[1] - e1[1];
-    return res === 0 ? e1[0].localeCompare(e2[0]) : res;
-}).forEach(e => console.log(`${e[0]} -> ${e[1]}`));
+const circle = {radius: 20, square: function(){
+    return 3.14 * (this.radius ** 2)
+}, perimeter: () => 2 * 3.14 * this.radius,
+toString: function(){
+    return `radius of this circle is ${this.radius}`
+}};
+console.log(`square = ${circle.square()}, perimeter = ${circle.perimeter()}`);
+// circle.square will be converted to square(circle) where circle as argument value,
+// this. is the hidden paramrtr in the function;
+//in any arrow function there is no "this";
+function square(circle){
+    return 3.14 * (circle.radius ** 2);
 }
-const ar = ["lmn", "d", "d", "lmn", "a", "lmn", "a", "bc"];
-displayOccurrences(ar);
+console.log(`circle: ${circle}`);
+const circle1 = {radius:20, perimeter: function(){
+    return 2 * 3.14 * this.radius}
+    //return 3.14 * (this.radius ** 2)}, perimeter: function(){
+    //    return 2 * 3.14 * this.radius},
+    ,toString: function(){
+        return `radius of this circle is ${this.radius}`
+    }};
+    //circle1.square(); //error because the method square is not defined inside object circle
+    //error: circle square is not a function;
+    function Circle(radius){
+        this.radius = radius;
+    }
+    Circle.prototype.square = function(){
+        return 3.14 * (this.radius ** 2);
+    }
+    Circle.prototype.perimeter = function(){
+        return 2 * 3.14 * this.radius;
+    }
+    Circle.prototype.toString =  function(){
+        return `radius of this circle is ${this.radius}`
+    }
+    const circle10 = new Circle(10);
+    //************************************************************** */
+    Array.prototype.filter = function(callbackPredicate){
+        console.log(`Tel-Ran copyright`);
+        const res = [];
+        this.forEach((n, i, a) => !callbackPredicate(n, i, a) && res.push(n));
+        return res;
+    }
+    const ar = [1, 2, 4, 5, 100];
+    ar.filter(n => n%2 === 0).forEach(n => console.log(n));
 
-//exapmles for homework-14 task-2
-//const arr = [6.4, 7.3, 6.5, 6.9];
-//const statistics = countBy(array, element => Math.floor(element))
-//result: -> {"6": 3, "7":1 }
-//const arr = ['abcd, 'lmnr', 'ab', 'dddd'];
-//const statistics = countBy(array, element => element.length)
-//result:statistics -> {"4": 3, "2": 1}
-//const arr = [{age: 25, id:123, name: "Vasya"}, {age: 50, id:123, name: "Vasya"}
-//{age: 25, id:123, name: "Vasya"}, {age: 70, id:123, name: "Vasya"}]
-//const statistics = countBy(array, element => element.age)
-//result: statistics -> {"25": 2, "50": 1, "70": 1};
 
 
